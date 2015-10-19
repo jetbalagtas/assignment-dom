8fdba20f876518ac0e04586b234f707c6c8d3291
Jet Balagtas <jetbalagtas@gmail.com>
2015-10-19 15:29:50 -0400

    added note to comment

--- main.js
+++ main.js
@@ -4,12 +4,11 @@
 // var readingTime = wordsArray.length / rwpm;
 //
 // document.getElementById('readTime').innerHTML = Math.round(readingTime);
-// var rwpm = 230;
+// ^my first instinct as a n00b was to write simple equation-type vars but it is not reusable, unlike below
 var readingTime = function (rwpm) {
   var totWords = document.body.textContent;
   var totSplitWords = totWords.split(" ");
   return totSplitWords.length / rwpm;
 }
-// return readingTime(230);
 
 document.getElementById('readTime').innerHTML = Math.round(readingTime(230));
